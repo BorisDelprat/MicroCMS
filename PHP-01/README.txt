0c66f9e1991569502ae8e5ca796cff7e05e0b912
Elodie <elodie.m@infonova.fr>
2015-10-12 15:58:09 +0200

    Quiz 4 fait : 10/10

--- PHP-01/README.txt
+++ PHP-01/README.txt
@@ -1,7 +1,7 @@
 Debut du cours PHP-01
 
 
-Temps passé : 4h20
+Temps passé : 8h20
 
 
 Quiz 1 : 10/10
@@ -10,4 +10,16 @@ Quiz 2 : 7/10
 
 Quiz 3 : 10/10
 
+Quiz 4 : 10/10
+
 Activité : en cours de correction
+
+
+Exp régulière pour les n° de téléphone FR:
+#^0[1-68][0-9]{8}$#			OK
+
+Pareil en prennant en compte les possibles tirets points et espaces
+#^0[1-68]([-. ]?[0-9]{2}){4}$#		OK
+
+Exp régulière pour les emails :
+#^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$#		OK
--- PHP-01/minichat.sql
+++ PHP-01/minichat.sql
@@ -1,3 +1,35 @@
-INSERT INTO `minichat` VALUES (33, 'Boris', 'Bonjour', '12 October 2015, 12:18:06');
-INSERT INTO `minichat` VALUES (34, 'Boris', 'ceci est un test', '12 October 2015, 12:18:13');
-INSERT INTO `minichat` VALUES (35, 'Boris', 'Le nom est enregistré dans un cookie puis ajouté dans le champ &quot;nom&quot;', '12 October 2015, 12:18:20');
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50617
+Source Host           : localhost:3306
+Source Database       : minichat
+
+Target Server Type    : MYSQL
+Target Server Version : 50617
+File Encoding         : 65001
+
+Date: 2015-10-12 13:12:34
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for minichat
+-- ----------------------------
+DROP TABLE IF EXISTS `minichat`;
+CREATE TABLE `minichat` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) DEFAULT NULL,
+  `message` varchar(255) DEFAULT NULL,
+  `datem` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of minichat
+-- ----------------------------
+INSERT INTO `minichat` VALUES ('2', 'Boris', 'tgrete', '12 October 2015, 13:11:48');
+INSERT INTO `minichat` VALUES ('3', 'Boris', 'test', '12 October 2015, 13:11:52');
+INSERT INTO `minichat` VALUES ('4', 'Boris', 'bonjour', '12 October 2015, 13:12:10');
